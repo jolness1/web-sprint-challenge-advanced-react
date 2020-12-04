6ea247593f44eca0cc8e11bd2ff53a5e537cd385
Jacob Olness <jacobolness@Jacobs-MacBook-Pro.local>
2020-12-04 09:52:04 -0700

    Got Plant List, Checkout and custom hook done

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -3,11 +3,21 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
-
+  state = {
+    plants: []
+  }
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
-
+  componentDidMount() {
+    axios.get('http://localhost:3333/plants')
+    .then((res) => {
+      this.setState({
+        plants: res.data.plantsData
+      });
+    })
+    .catch(err=> console.log(err));
+  }
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
     return (
@@ -18,7 +28,7 @@ export default class PlantList extends Component {
             <div className="plant-details">
               <h2 className="plant-name">{plant.name}</h2>
               <p className="plant-scientific-name">{plant.scientificName}</p>
-              <p>{plant.description}</p>
+              <p>{plant.description}</p>y
               <div className="plant-bottom-row">
                 <p>${plant.price}</p>
                 <p>☀️ {plant.light}</p>
