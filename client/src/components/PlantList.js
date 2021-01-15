3275504fb815c1627d7bb0b89a7c9169ffe481f6
Jacob Olness <jacobolness@Jacobs-MBP.lan>
2021-01-15 09:30:11 -0700

    cleaned up useForm to redo it

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -3,10 +3,12 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
-  state ={
-    plants: []
+  constructor(){
+    super()
+      this.state ={
+        plants: []
+      }
   }
-
   componentDidMount(){
   axios
     .get("http://localhost:3333/plants")
